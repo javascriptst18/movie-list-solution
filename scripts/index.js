ae55abf40fd714d2f04a2c360ee2c498264c079b
Jesper Orb <jesper.orb@zocom.se>
2018-06-25 13:24:11 +0200

    Extract html creation into separate function

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,22 @@
-
 function getMovies(search = '') {
   let url = `https://javascriptst18.herokuapp.com/trending?q=${search}`;
   $.getJSON(url, function (allMovies) {
-    const container = document.querySelector('.container');
-    let html = '';
-    for (let movie of allMovies) {
-      html += `
+    createHTML(allMovies);
+  })
+}
+
+function createHTML(allMovies) {
+  const container = document.querySelector('.container');
+  let html = '';
+  for (let movie of allMovies) {
+    html += `
         <div class="card">
           <p>${movie.title}</p>
           <p>${movie.genres} </p>
         </div>
       `;
-    }
-    container.innerHTML = html;
-  })
-
+  }
+  container.innerHTML = html;
 }
 
 function addSearchEvent() {
